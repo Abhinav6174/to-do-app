b2ee6ff7ad8d7c54e62d596856f0ae2c01e5780b
ABHINAV <abhinav.vadlamudi@gmail.com>
2024-01-08 18:12:38 +0530

    Added Edit Functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import './App.css'
-import { MdDeleteOutline, MdCheck } from "react-icons/md";
-
+import { MdDeleteOutline, MdCheck, MdEdit } from "react-icons/md" 
 
 const App = () => {
   const[isCompleteScreen, setIsCompleteScreen] = useState(false)
@@ -9,49 +8,92 @@ const App = () => {
   const[newTitle, setNewTitle] = useState("")
   const[newDescription, setNewDescription] = useState("")
   const[CompletedTodos, setCompletedTodos] = useState([])
+  const[editMode, setEditMode] = useState(false) 
+  const[editedIndex, setEditedIndex] = useState(null) 
+  const isAddButtonDisabled = !newTitle.trim() || !newDescription.trim() 
 
-  const addTodoHandle = () =>{
-    let newTodoItem = {
-      title:newTitle,
-      description:newDescription
-    }
-    let updatedTodoArray = [...allTodos]
-    updatedTodoArray.push(newTodoItem)
-    setAllTodos(updatedTodoArray)
-    localStorage.setItem('todoList', JSON.stringify(updatedTodoArray))
-    setNewTitle("")
-    setNewDescription("")
-  }
-
-  const deleteTodoHandle = (index) =>{
-    let deletedTodoArray = [...allTodos]
-    deletedTodoArray.splice(index, 1)
-    setAllTodos(deletedTodoArray)
-    localStorage.setItem('todoList', JSON.stringify(deletedTodoArray))
-  }
-
-
-  const completedTodoHandle = (index) =>{
-    let filteredItem = {
-      ...allTodos[index]
-    }
-    let completedTodoArray = [...CompletedTodos]
-    completedTodoArray.push(filteredItem)
-    setCompletedTodos(completedTodoArray)
-    deleteTodoHandle(index) // Delete item from actual todo list
-    localStorage.setItem('completedTodoList', JSON.stringify(completedTodoArray))
-  }
-
+  //Adds Todo
+  const addTodoHandle = () => {
+    let updatedTodoArray 
   
+    if (editMode && editedIndex !== null) {
+      // Editing existing Todo
+      updatedTodoArray = [...allTodos] 
+      updatedTodoArray[editedIndex] = {
+        title: newTitle,
+        description: newDescription,
+      } 
+      setEditMode(false) 
+      setEditedIndex(null) 
+    } 
+    else {
+      // Adding new Todo
+      const newTodoItem = {
+        title: newTitle,
+        description: newDescription,
+      } 
+      updatedTodoArray = [...allTodos, newTodoItem] 
+    }
+    setAllTodos(updatedTodoArray) 
+    localStorage.setItem('todoList', JSON.stringify(updatedTodoArray)) 
+  
+    setNewTitle('') 
+    setNewDescription('') 
+  } 
+
+  // edit specific Todo
+  const editTodoHandle = (index) => {
+    setEditMode(true) 
+    setEditedIndex(index) 
+    setNewTitle(allTodos[index].title) 
+    setNewDescription(allTodos[index].description) 
+  } 
+
+  // Deletes Todo
+  const deleteTodoHandle = (index, showConfirmation = true) => {
+    if (showConfirmation) {
+      const confirmDeletion = window.confirm('Are you sure you want to delete this todo?') 
+      if (!confirmDeletion) {
+        return 
+      }
+    }
+
+    let deletedTodoArray = [...allTodos] 
+    deletedTodoArray.splice(index, 1) 
+    setAllTodos(deletedTodoArray) 
+    localStorage.setItem('todoList', JSON.stringify(deletedTodoArray)) 
+  }
+
+  // Adds completed todo to completed tab
+  const completedTodoHandle = (index) => {
+    const confirmCompletion = window.confirm('Mark this todo as completed?')
+    if (confirmCompletion) {
+      let filteredItem = {
+        ...allTodos[index]
+      }
+
+      let completedTodoArray = [...CompletedTodos] 
+      completedTodoArray.push(filteredItem) 
+      setCompletedTodos(completedTodoArray) 
+      // Delete item from the actual todo list without confirmation
+      deleteTodoHandle(index, false) 
+      localStorage.setItem('completedTodoList', JSON.stringify(completedTodoArray))
+    }
+  }
+  
+  //Deletes Completed Todos
   const deleteCompletedTodoHandle = (index) =>{
-    let deleteCompletedTodoArray = [...CompletedTodos]
-    deleteCompletedTodoArray.splice(index, 1)
-    setCompletedTodos(deleteCompletedTodoArray)
-    localStorage.setItem('completedTodoList', JSON.stringify(deleteCompletedTodoArray))
+    const confirmDeletion = window.confirm('Are you sure you want to delete this Completed Todo?') 
+    if (confirmDeletion) {
+      let deleteCompletedTodoArray = [...CompletedTodos]
+      deleteCompletedTodoArray.splice(index, 1)
+      setCompletedTodos(deleteCompletedTodoArray)
+      localStorage.setItem('completedTodoList', JSON.stringify(deleteCompletedTodoArray))
+    }
   }
 
   //using useEffect hook whenever the page is rendered 1st and checking whether local storage contains item or not
-
+  //session storage
   useEffect(() =>{
     let localTodo = JSON.parse(localStorage.getItem('todoList'))
     if(localTodo){
@@ -71,17 +113,17 @@ const App = () => {
         <div className='todo-input'>
 
           <div className='todo-input-item'>
-            <label>Title</label>
-            <input type='text' value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder='Enter Title' />
+            <label>Title: </label>
+            <input type='text' value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder='Enter Todo Title' required/>
           </div>
 
           <div className='todo-input-item'>
-            <label>Description</label>
-            <input type='text' value={newDescription} onChange={(e) => setNewDescription(e.target.value)} placeholder='Enter Description' />
+            <label>Description: </label>
+            <input type='text' value={newDescription} onChange={(e) => setNewDescription(e.target.value)} placeholder='Enter Todo Description' required/>
           </div>
 
-          <div className='todo-input-item'>
-            <button type='button' onClick={addTodoHandle} className='primarybtn'>Add</button>
+          <div className='todo-input-itm'>
+            <button type='button' onClick={addTodoHandle} className='addbtn' disabled={isAddButtonDisabled}>Add Todo</button>
           </div>
         </div>
 
@@ -95,12 +137,13 @@ const App = () => {
             return(
               <div className='todo-list-item' key={index}>
                 <div>
-                  <h3>{item.title}</h3>
-                  <p>{item.description}</p>
+                  <h3 className='todo-input-item-text'>{item.title}</h3>
+                  <p className='todo-input-item-text'>{item.description}</p>
                 </div>
                 <div>
-                  <MdDeleteOutline className='icon' onClick={() => deleteTodoHandle(index)} title='Delete?' />
-                  <MdCheck className='check-icon' onClick={() => completedTodoHandle(index)} title='Complete?'/>
+                  <MdDeleteOutline className='icons' onClick={() => deleteTodoHandle(index)} title='Delete' />
+                  <MdCheck className='icons' onClick={() => completedTodoHandle(index)} title='Complete'/>
+                  <MdEdit className='icons' onClick={() => editTodoHandle(index)} title='Edit' />
                 </div>
               </div>
             )
@@ -110,44 +153,16 @@ const App = () => {
             return(
               <div className='todo-list-item' key={index}>
                 <div>
-                  <h3>{item.title}</h3>
-                  <p>{item.description}</p>
+                  <h3 className='todo-input-item-text'>{item.title}</h3>
+                  <p className='todo-input-item-text'>{item.description}</p>
                 </div>
                 <div>
-                  <MdDeleteOutline className='icon' onClick={() => deleteCompletedTodoHandle(index)} title='Delete?' />
-                  {/* <MdCheck className='check-icon' onClick={() => completedTodoHandle(index)} title='Complete?'/> */}
+                  <MdDeleteOutline className='icons' onClick={() => deleteCompletedTodoHandle(index)} title='Delete?' />
                 </div>
               </div>
             )
           })}
-
-
-
-
-
-
-
-
-
-
-
-
-          <div className='todo-list-item'>
-            <div>
-              <h3>Task 1</h3>
-              <p>Description</p>
-            </div>
-            <div>
-              <MdDeleteOutline className='icon' />
-              <MdCheck className='check-icon' />
-            </div>
-          </div>
         </div>
-
-
-
-
-
       </div>
     </div>
   )
